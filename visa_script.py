4797d6989b905877775e3e2fd482d07b515c76a8
kiuran <kiuran@users.noreply.github.com>
2017-02-23 15:35:14 +0200

    Update visa_script.py

--- visa_script.py
+++ visa_script.py
@@ -1,3 +1,4 @@
+#Kiuran Naidoo
 import visa
 import datetime
 import time
@@ -9,11 +10,10 @@ if __name__ == "__main__":
     print "Connected to:"
     print device
     print(device.query("*IDN?"))
-    print(device.query("SENS:FREQ:STAR?"))
     device.write("MMEM:STOR:FDAT \"MyFile.csv\"")
-    k =  device.query("MMEM:DATA? \"MyFile.csv\"")
+    data =  device.query("MMEM:DATA? \"MyFile.csv\"")
     device.write("MMEM:DEL \"MyFile.csv\"")
     times = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H.%M.%S')
     file_ = open('Data: ' + times + '.csv', 'w')
-    file_.write(k)
+    file_.write(data)
     file_.close()
